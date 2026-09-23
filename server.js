9f521fa1951b881ca06349632b93b4b34acd6b75
asmile2080-boop <asmile2080@gmail.com>
2026-09-23 13:23:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const {
   requireAdmin,
   sendJson,
 } = require("./auth");
-const paymentProvider = require("./providers/paymentProvider");
-const vtuProvider = require("./providers/vtuProvider");
+const paymentProvider = require(".paymentProvider");
+const vtuProvider = require(".vtuProvider");
 const { verifyPin, reserveFunds, settleTransaction } = require("./purchaseEngine");
 const { settleFunding } = require("./fundingEngine");
 const { startReconciliationLoop, reconcileNow } = require("./reconcile");
